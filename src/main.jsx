0ba23057fc30747edea0315d81453b358c674563
KritAsawaniramol <krit.as@ku.th>
2024-10-20 15:06:37 +0700

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { ThemeContextProvider } from './theme/ThemeContextProvider.jsx'
 import CartContextProvider from './CartContext.jsx'
 import MyCart from './MyCart.jsx'
 import MyOrder from './MyOrder.jsx'
-import MyShelf from './Myshelf.jsx'
+
 import ReadBook from './ReadBook.jsx'
 import CreateBook from './CreateBook.jsx'
 import UserTransactions from './UserTransactions.jsx'
@@ -17,6 +17,7 @@ import RequireAuth from './auth/RequireAuth.jsx'
 import Book from './Book.jsx'
 import AdminBook from './AdminBook.jsx'
 import UpdateBook from './UpdateBook.jsx'
+import MyShelf from './Myshelf.jsx'
 
 const router = createBrowserRouter([
   {
